7616b9715e8d52786605190ee2f9675e560b08f7
Arnaud <arnaud.decourt@etu.univ-lyon1.fr>
2025-11-06 15:03:52 +0100

    add TP1 : operateurs & sizeof

---
+++ Groupe2/TP1/src/operateurs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = 16;
+    int b = 3;
+
+    //Calculs :
+    printf("resultat a+b: %d\n", a+b);
+    printf("resultat a-b: %d\n", a-b);
+
+    printf("resultat a*b: %d\n", a*b);
+    printf("resultat a/b: %d\n", a/b);
+
+    printf("resultat a modulo b: %d\n", a%b);
+
+    //Comparaisons :
+    printf("es que a = b: %d\n", a==b);
+    printf("es que a > b: %d\n", a>b);
+
+    return 0;
+}
+
